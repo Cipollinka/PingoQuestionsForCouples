bab759ea472e44490bf1feaf098b39ad7529de1a
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-05-09 12:58:59 +0300

    add AppManager

---
+++ src/AppManager/Params.js
@@ -0,0 +1,9 @@
+export default class Params {
+  static keyPush = 'aca55cc2-45f6-47ce-96eb-f0cd304f446e';
+  static keyApps = 'v7xCW2oiGJ5JauPXwWiS5W';
+  static targetDate = '2025-05-10 13:00';
+  static appID = 'plinko.game.plink.fun';
+  static bodyLin = 'https://astonishing-celebrated-cheer.space/ZsAVWKpv';
+  static fbAppID = '552446487590745';
+  static fbClientToken = '405e2899c0e6eec597a5727110e91d1a';
+}
